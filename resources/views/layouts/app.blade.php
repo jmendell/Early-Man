72b2413cab990d08bdd675b75bf580b6b83e7fc0
Jake Mendell <jmendell4@gmail.com>
2017-12-21 13:42:02 -0700

    share image added in head file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 	<!-- for Facebook -->          
 	<meta property="og:title" content="Early Man - Ecard Generator" />
 	<meta property="og:type" content="video.movie" />
-	<meta property="og:image" content="/images/share-image.jpg" />
+	<meta property="og:image" content="http://earlymanecards.com/images/share-image.jpg" />
 	<meta property="og:url" content="http://earlymanecards.com/" />
 	<meta property="og:description" content="Early Man - Ecard Generator. Send a holiday card from the stone age" />
 	<!-- for Twitter -->          
@@ -19,7 +19,7 @@
 	<meta name="twitter:title" content="Early Man - Ecard Generator" />
 	<meta name="twitter:site" content="http://earlymanecards.com/">
 	<meta name="twitter:description" content="Early Man Ecard Generator. Send a holiday card from the stone age." />
-	<meta name="twitter:image" content="" />
+	<meta name="twitter:image" content="http://earlymanecards.com/images/share-image.jpg" />
 	<link rel="icon" href="images/earlyman_fav_icon.png" type="image/x-icon" />
 	<title>Early Man - Ecard Generator</title>
 	<link rel="canonical" href="">
@@ -60,122 +60,6 @@
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 </script>
-
-<?php 
-	// Generate a login URL
-	Route::get('/facebook/login', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb)
-	{
-	    // Send an array of permissions to request
-	    $login_url = $fb->getLoginUrl(['email']);
-
-	    // Obviously you'd do this in blade :)
-	    echo '<a href="' . $login_url . '">Login with Facebook</a>';
-	});
-
-	// Endpoint that is redirected to after an authentication attempt
-	Route::get('/facebook/callback', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb)
-	{
-	    // Obtain an access token.
-	    try {
-	        $token = $fb->getAccessTokenFromRedirect();
-	    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-	        dd($e->getMessage());
-	    }
-
-	    // Access token will be null if the user denied the request
-	    // or if someone just hit this URL outside of the OAuth flow.
-	    if (! $token) {
-	        // Get the redirect helper
-	        $helper = $fb->getRedirectLoginHelper();
-
-	        if (! $helper->getError()) {
-	            abort(403, 'Unauthorized action.');
-	        }
-
-	        // User denied the request
-	        dd(
-	            $helper->getError(),
-	            $helper->getErrorCode(),
-	            $helper->getErrorReason(),
-	            $helper->getErrorDescription()
-	        );
-	    }
-
-	    if (! $token->isLongLived()) {
-	        // OAuth 2.0 client handler
-	        $oauth_client = $fb->getOAuth2Client();
-
-	        // Extend the access token.
-	        try {
-	            $token = $oauth_client->getLongLivedAccessToken($token);
-	        } catch (Facebook\Exceptions\FacebookSDKException $e) {
-	            dd($e->getMessage());
-	        }
-	    }
-
-	    $fb->setDefaultAccessToken($token);
-
-	    // Save for later
-	    Session::put('fb_user_access_token', (string) $token);
-
-	    // Get basic info on the user from Facebook.
-	    try {
-	        $response = $fb->get('/me?fields=id,name,email');
-	    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-	        dd($e->getMessage());
-	    }
-
-	    // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
-	    $facebook_user = $response->getGraphUser();
-
-	    // Create the user if it does not exist or update the existing entry.
-	    // This will only work if you've added the SyncableGraphNodeTrait to your User model.
-	    $user = App\User::createOrUpdateGraphNode($facebook_user);
-
-	    // Log the user into Laravel
-	    Auth::login($user);
-
-	    return redirect('/')->with('message', 'Successfully logged in with Facebook');
-	});
-
-	Route::get('/facebook/login', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
-    $login_link = $fb
-            ->getRedirectLoginHelper()
-            ->getLoginUrl('https://exmaple.com/facebook/callback', ['email', 'user_events']);
-    
-    echo '<a href="' . $login_link . '">Log in with Facebook</a>'; });
-
-    $login_link = $fb->getLoginUrl();
-
-    $login_link = $fb->getLoginUrl(['email', 'user_status'], 'http://earlymanecards.com/facebook/callback');
-	// Or, if you want to default to the callback URL set in the config
-	$login_link = $fb->getLoginUrl(['email', 'user_status']);
-
-	Route::get('/facebook/callback', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
-		try {
-		    $token = $fb
-		        ->getRedirectLoginHelper()
-		        ->getAccessToken();
-		} catch (Facebook\Exceptions\FacebookSDKException $e) {
-		    // Failed to obtain access token
-		    dd($e->getMessage());
-		}
-	});
-
-	Route::get('/facebook/callback', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
-	    try {
-	        $token = $fb->getAccessTokenFromRedirect();
-	    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-	        // Failed to obtain access token
-	        dd($e->getMessage());
-	    }
-	    
-	    // $token will be null if the user denied the request
-	    if (! $token) {
-	        // User denied the request
-	    }
-	});
-?>
 
 	<main id="main">
 
