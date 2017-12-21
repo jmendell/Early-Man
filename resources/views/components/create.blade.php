2194e2cc8fd63a63040706a558f2894a76f83a62
Jake Mendell <jmendell4@gmail.com>
2017-12-21 11:08:09 -0700

    Removed duplication of canvas textbox

--- resources/views/components/create.blade.php
+++ resources/views/components/create.blade.php
@@ -21,24 +21,5 @@ From: Me</textarea>
 				{{ csrf_field() }}
 			</div>
 		</div>
-		<div class="col-6 textbox-col-mobile">
-			<div class="form-wrap">
-				<div class="field-group">
-					<h3>ENTER YOUR MESSAGE</h3>
-					<span class="character-amount">(Limit 50 characters)</span>
-					<textarea id="content-input" rv-text="content">To: You
-From: Me</textarea>
-				<p class="counter">CHARACTERS REMAINING: <span id="charCount">50</span></p>
-				</div>
-				<input type="submit" id="finalize-creation" value="See Your Card">
-				{{ csrf_field() }}
-			</div>
-			<div class="progress-section">
-				<h4>GENERATING YOUR ECARD!</h4>
-				<div class="progress">
-					<div class="progress-inner"></div>
-				</div>
-			</div>
-		</div>
 	</div>
 </section>
