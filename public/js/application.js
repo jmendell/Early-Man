19d872c58e64e9fc69ac441c7c353faf09d3c79a
Jake Mendell <jmendell4@gmail.com>
2017-12-20 20:13:35 -0700

    changed width of text typing on canvas for cards

--- public/js/application.js
+++ public/js/application.js
@@ -161,7 +161,7 @@ $app.cards = {};
 $app.cards = {
 	card1 : function () {
 		$app.baseUrl = "/images/cards/card-1.jpg";
-		$app.text.width = 400;
+		$app.text.width = 800;
 		$app.text.top = 400;
 		$app.text.left = 50;
 		$app.text.fill = "#000";
@@ -169,7 +169,7 @@ $app.cards = {
 	},
 	card2 : function () {
 		$app.baseUrl = "/images/cards/card-2.jpg";
-		$app.text.width = 350;
+		$app.text.width = 800;
 		$app.text.top = 50;
 		$app.text.left = 25;
 		$app.text.fill = "#0D2842";
@@ -177,7 +177,7 @@ $app.cards = {
 	},
 	card3 : function () {
 		$app.baseUrl = "/images/cards/card-3.jpg";
-		$app.text.width = 850;
+		$app.text.width = 800;
 		$app.text.top = 725;
 		$app.text.left = 500;
 		$app.text.fill = "#000";
@@ -193,7 +193,7 @@ $app.cards = {
 	},
 	card5 : function () {
 		$app.baseUrl = "/images/cards/card-5.jpg";
-		$app.text.width = 850;
+		$app.text.width = 800;
 		$app.text.top = 650;
 		$app.text.left = 50;
 		$app.text.fill = "#fff";
@@ -201,7 +201,7 @@ $app.cards = {
 	},
 	card6 : function () {
 		$app.baseUrl = "/images/cards/card-6.jpg";
-		$app.text.width = 400;
+		$app.text.width = 800;
 		$app.text.top = 600;
 		$app.text.left = 500;
 		$app.text.fill = "#fff";
@@ -209,7 +209,7 @@ $app.cards = {
 	},
 	card7 : function () {
 		$app.baseUrl = "/images/cards/card-7.jpg";
-		$app.text.width = 850
+		$app.text.width = 800
 		$app.text.top = 650;
 		$app.text.left = 50;
 		$app.text.fill = "#fff";
@@ -217,7 +217,7 @@ $app.cards = {
 	},
 	card8 : function () {
 		$app.baseUrl = "/images/cards/card-8.jpg";
-		$app.text.width = 600;
+		$app.text.width = 800;
 		$app.text.top = 50;
 		$app.text.left = 50;
 		$app.text.fill = "#0D2842";
@@ -249,7 +249,7 @@ $app.cards = {
 	},
 	card12 : function () {
 		$app.baseUrl = "/images/cards/card-12.jpg";
-		$app.text.width = 350;
+		$app.text.width = 800;
 		$app.text.top = 25;
 		$app.text.left = 25;
 		$app.text.fill = "#fff";
--- resources/assets/js/application/card-profiles.js
+++ resources/assets/js/application/card-profiles.js
@@ -3,7 +3,7 @@ $app.cards = {};
 $app.cards = {
 	card1 : function () {
 		$app.baseUrl = "/images/cards/card-1.jpg";
-		$app.text.width = 400;
+		$app.text.width = 800;
 		$app.text.top = 400;
 		$app.text.left = 50;
 		$app.text.fill = "#000";
@@ -11,7 +11,7 @@ $app.cards = {
 	},
 	card2 : function () {
 		$app.baseUrl = "/images/cards/card-2.jpg";
-		$app.text.width = 350;
+		$app.text.width = 800;
 		$app.text.top = 50;
 		$app.text.left = 25;
 		$app.text.fill = "#0D2842";
@@ -19,7 +19,7 @@ $app.cards = {
 	},
 	card3 : function () {
 		$app.baseUrl = "/images/cards/card-3.jpg";
-		$app.text.width = 850;
+		$app.text.width = 800;
 		$app.text.top = 725;
 		$app.text.left = 500;
 		$app.text.fill = "#000";
@@ -35,7 +35,7 @@ $app.cards = {
 	},
 	card5 : function () {
 		$app.baseUrl = "/images/cards/card-5.jpg";
-		$app.text.width = 850;
+		$app.text.width = 800;
 		$app.text.top = 650;
 		$app.text.left = 50;
 		$app.text.fill = "#fff";
@@ -43,7 +43,7 @@ $app.cards = {
 	},
 	card6 : function () {
 		$app.baseUrl = "/images/cards/card-6.jpg";
-		$app.text.width = 400;
+		$app.text.width = 800;
 		$app.text.top = 600;
 		$app.text.left = 500;
 		$app.text.fill = "#fff";
@@ -51,7 +51,7 @@ $app.cards = {
 	},
 	card7 : function () {
 		$app.baseUrl = "/images/cards/card-7.jpg";
-		$app.text.width = 850
+		$app.text.width = 800
 		$app.text.top = 650;
 		$app.text.left = 50;
 		$app.text.fill = "#fff";
@@ -59,7 +59,7 @@ $app.cards = {
 	},
 	card8 : function () {
 		$app.baseUrl = "/images/cards/card-8.jpg";
-		$app.text.width = 600;
+		$app.text.width = 800;
 		$app.text.top = 50;
 		$app.text.left = 50;
 		$app.text.fill = "#0D2842";
@@ -91,7 +91,7 @@ $app.cards = {
 	},
 	card12 : function () {
 		$app.baseUrl = "/images/cards/card-12.jpg";
-		$app.text.width = 350;
+		$app.text.width = 800;
 		$app.text.top = 25;
 		$app.text.left = 25;
 		$app.text.fill = "#fff";
