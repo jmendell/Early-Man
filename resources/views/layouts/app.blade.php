27d3e16537ed00d9df67b6176a794776e7216280
Jake Mendell <jmendell4@gmail.com>
2017-12-21 11:15:25 -0700

    facebook sdk again

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,25 @@
 	</script>
 </head>
 <body class="@yield('bodyClass')" style="background-image:url('/images/earlyman-bg.jpg');">
+<!-- FB SDK -->
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '2065661193664389',
+      xfbml      : true,
+      version    : 'v2.11'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 
 	<main id="main">
 
